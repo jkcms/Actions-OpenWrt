8d475002ba96a1940516fcc4a28e9e6de0a743e5
jkcms <chenjingkuang@gmail.com>
2023-01-20 14:05:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,6 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 #echo "src-git cups https://github.com/Gr4ffy/lede-cups.git" >> feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git small8 https://github.com/kenzok8/small-package" >> feeds.conf.default
+echo "src-git lienol https://github.com/Lienol/openwrt-packages" >> feeds.conf.default
+echo "src-git Aibx https://github.com/Aibx/openwrt-packages" >> feeds.conf.default
